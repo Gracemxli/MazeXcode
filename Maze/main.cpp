57efca327193912026f1617d2957ab6b0ecf8bba
Gracemxli <gracemxli51@gmail.com>
2023-10-13 18:56:39 -0700

    update

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -1,14 +1,77 @@
-//
-//  main.cpp
-//  Maze
-//
-//  Created by Grace Li on 10/13/23.
-//
+/*
+maze.cpp
+*/
 
 #include <iostream>
+#include "mazeio.h"
+// #include "queue.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+// Prototype for maze_search, which you will fill in below.
+int maze_search(char**, int, int);
+
+// Add other prototypes here for any functions you wish to use
+
+
+// main function to read, solve maze, and print result
+int main(int argc, char* argv[]) {
+    int rows, cols, result;
+    char** mymaze=NULL;
+    const char* invalid_char_message = "Error, invalid character.";
+    const char* invalid_maze_message = "Invalid maze.";
+    const char* no_path_message = "No path could be found!";
+
+    if(argc < 2)
+    {
+        cout << "Please provide a maze input file" << endl;
+        return 1;
+    }
+
+    mymaze = read_maze... // <---TASK: COMPLETE THIS FOR CHECKPOINT 1
+
+    // For checkpoint 2 you should check the validity of the maze
+    // You may do so anywhere you please and can abstract that
+    // operation with a function or however you like.
+
+
+
+    //================================
+    // When working on Checkpoint 4, you will need to call maze_search
+    // and output the appropriate message or, if successful, print
+    // the maze.  But for Checkpoint 1, we print the maze, regardless.
+    print_maze(mymaze, rows, cols);
+    
+
+    //================================
+    // ADD CODE BELOW
+    // to delete all memory that read_maze allocated: CHECKPOINT 2
+
+
+
+
+
     return 0;
+}
+
+/**************************************************
+ * Attempt to find shortest path and return:
+ *  1 if successful
+ *  0 if no path exists
+ *
+ * If path is found fill it in with '*' characters
+ *  but don't overwrite the 'S' and 'F' cells
+ * NOTE: don't forget to deallocate memory in here too!
+ *************************************************/
+int maze_search(char** maze, int rows, int cols)
+{
+    // *** You complete **** CHECKPOINT 4
+
+
+
+
+
+
+
+    return 0; // DELETE this stub, it's just for Checkpoint 1 to compile.
 }
---
+++ Maze/mazio.hpp
@@ -0,0 +1,13 @@
+//
+//  mazio.hpp
+//  Maze
+//
+//  Created by Grace Li on 10/13/23.
+//
+
+#ifndef mazio_hpp
+#define mazio_hpp
+
+#include <stdio.h>
+
+#endif /* mazio_hpp */
